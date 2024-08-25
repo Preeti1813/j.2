5f78da51ef8a77bf7520b49a186ee699fdbe15a8
Preeti1813 <zi1989166@gmail.com>
2024-08-26 02:05:23 +0600

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,31 @@
+let a = 20;
+  let b = 15;
+  
+ let total = a + b;
+ 
+ 
+ 
+
+ 
+ document.getElementById('demo').innerHTML = total;
+ 
+ let total1 = a * b;
+ 
+ document.getElementById('demo1').innerHTML = total1;
+ 
+ let total2 = a - b;
+ 
+ document.getElementById('demo2').innerHTML = total2;
+ 
+ let total3 = a / b;
+ 
+ document.getElementById('demo3').innerHTML = total3;
+ 
+ 
+ 
+ let ab = 'Hello';
+ let bc = 'Nactar';
+ 
+ let result = ab + ' ' + bc;
+ 
+ document.getElementById('demo4').innerHTML = result;
